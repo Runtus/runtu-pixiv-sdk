01ff5960bb172570ccc0e67e24c71cf3674c4837
Runtus <893119806@qq.com>
2022-08-08 10:56:41 +0800

    ✨ feat(author): add author search

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -5,16 +5,16 @@ import { Search } from './search'
 
 const routers = new Router();
 
-routers.get('/ranks/day', DayRanks);
-routers.get('/ranks/week', WeekRanks);
-routers.get('/ranks/month', MonthRanks);
+routers.get('/ranks/day', DayRanks)
+routers.get('/ranks/week', WeekRanks)
+routers.get('/ranks/month', MonthRanks)
 
 // 因为pixiv-cdn的存在，不需要oss进行中转存储
 // routers.get('/oss/day', OssDayRanks);
 // routers.get('/oss/week', OssWeekRanks);
 // routers.get('/oss/month', OssMonthRanks);
 
-routers.get('/illusts/search', Search.illust);
+routers.get('/illusts/search', Search.illust)
 routers.get('/author/search', Search.author)
 
 export default routers;
