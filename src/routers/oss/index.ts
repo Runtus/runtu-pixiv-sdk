3cf4a8815fa666657e2ace7b10dbaf922b1b7301
Runtus <893119806@qq.com>
2022-08-11 09:35:37 +0800

    ✨ feat:

--- src/routers/oss/index.ts
+++
@@ -1,6 +0,0 @@
-import { OssUrls } from './core';
-import { RankingMode } from '@src/request/type';
-
-export const OssDayRanks = OssUrls(RankingMode.DAY);
-export const OssWeekRanks = OssUrls(RankingMode.WEEK);
-export const OssMonthRanks = OssUrls(RankingMode.MONTH);
