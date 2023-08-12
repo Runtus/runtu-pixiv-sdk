fb1623d296d26069779496cbf47f12aa4d045ba1
Runtus <893119806@qq.com>
2023-08-12 15:37:07 +0800

    add response rype

--- src/routers/ranking/core.ts
+++ src/routers/ranking/core.ts
@@ -1,17 +1,8 @@
-import { RankingMode } from '@src/request/type';
+import { PixivResponse, RankingMode, WebPixivType } from '@src/request/type';
 import { getRanks } from '@src/request/ranking/getRanks';
-import { RPixivData } from './../type'
 
-export const RanksCore: (mode: RankingMode) => (token: string, range: string) => Promise<RPixivData> = (mode) => {
+export const RanksCore: (mode: RankingMode) => (token: string, range: string) => PixivResponse<WebPixivType> = (mode) => {
     return async (token, range) => {
-        const { data: pixivImages, date } = await getRanks(mode, token, range);
-        return {
-            code: 200,
-            data: {
-                date,
-                illusts: pixivImages.illusts,
-            },
-            info: pixivImages.illusts.length ? '获取图片成功' : '获取图片失败',
-        };
+        return  getRanks(mode, token, range)
     };
 };
